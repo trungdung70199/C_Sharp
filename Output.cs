798e6cb9aab164807970ef0aa8d67715806a5efd
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2023-02-16 11:56:33 +0900

    Hello world in C#

---
+++ Output.cs
@@ -0,0 +1,43 @@
+// To output values or print text C# use the WriteLine()
+// Can add as many WriteLine()
+
+using System
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriterLine("I am lerning C#");
+            Console.WriteLine("It is awesome");
+        }
+    }
+}
+// Can also output numbers and perform mathematical calculations
+using System
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 + 3)
+        }
+    }
+}
+
+// The Write Method
+// Also a Write() method, which is similar to WriteLine()
+using System
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("I will print on the line");
+        }
+    }
+}
